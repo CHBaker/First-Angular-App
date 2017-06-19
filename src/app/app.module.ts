e70d78f98ecd42a8a8e6f41e970b6100cf238798
Charles Baker <charlesprobaker@gmail.com>
2017-06-19 12:46:40 -0600

    ADD success and fail message components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningMessageComponent} from './warningMessage/warningMessage.component';
+import { SuccesMessageComponent } from './succes-message/succes-message.component';
+import { SuccessMessageComponent } from './success-message/success-message.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningMessageComponent,
+    SuccesMessageComponent,
+    SuccessMessageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warningMessage/warningMessage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warningMessage',
+    templateUrl: './warningMessage.component.html',
+    styleUrls: ['./warningMessage.component.css']
+})
+
+export class WarningMessageComponent{}
