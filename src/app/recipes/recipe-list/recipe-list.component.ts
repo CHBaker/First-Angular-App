782531d06358e1b51fc0172974a0720109d2aa71
Charles Baker <charlesprobaker@gmail.com>
2017-06-20 08:34:08 -0600

    ADD recipe list menu

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('a Testipe', 'this is a test',
+        'http://spoonforkbacon.com/wordpress/wp-content/uploads/2012/08/achiote-grilled-fish-taco-recipe1.jpg')
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
