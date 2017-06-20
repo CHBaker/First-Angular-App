c182696120853d2ac619585a275f13c3068cf366
Charles Baker <charlesprobaker@gmail.com>
2017-06-20 07:55:35 -0600

    ADD dropdown menu and header REMOVE .ts files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeComponent } from './recipes/recipe-list/recipe/recipe.component';
 import { IngredientComponent } from './shopping/shopping-list/ingredient/ingredient.component';
+import { ShoppingEditComponent} from './shopping/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { IngredientComponent } from './shopping/shopping-list/ingredient/ingredi
     RecipeListComponent,
     RecipeComponent,
     IngredientComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
