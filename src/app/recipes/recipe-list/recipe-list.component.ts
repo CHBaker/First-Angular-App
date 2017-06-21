189d01d717cc14b2e5862ea824d5d999db334f11
Charles Baker <charlesprobaker@gmail.com>
2017-06-21 12:36:16 -0600

    RELOAD files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
 @Component({
-    selector: 'app-recipe-list',
-    templateUrl: './recipe-list.component.html',
-    styleUrls: ['./recipe-list.component.css']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-    @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
 
-    recipes: Recipe[] = [
-        new Recipe('a Testipe', 'this is a test',
-        'http://spoonforkbacon.com/wordpress/wp-content/uploads/2012/08/achiote-grilled-fish-taco-recipe1.jpg'),
-        new Recipe('a Testipe', 'this is a test',
-        'http://spoonforkbacon.com/wordpress/wp-content/uploads/2012/08/achiote-grilled-fish-taco-recipe1.jpg')
-    ];
+  constructor() { }
 
-    constructor() { }
+  ngOnInit() {
+  }
 
-    ngOnInit() {
-    }
+  onRecipeSelected(recipe: Recipe) {
+   this.recipeWasSelected.emit(recipe);
+  }
 
-    onRecipeSelected(recipe: Recipe) {
-        this.recipeWasSelected.emit(recipe);
-    }
 }
