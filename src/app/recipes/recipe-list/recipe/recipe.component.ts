57189d5cc367472b8217bbc16662db7958ca2424
Charles Baker <charlesprobaker@gmail.com>
2017-06-21 10:10:13 -0600

    ADD re structure recipe model data, nested in the recipe list

--- src/app/recipes/recipe-list/recipe/recipe.component.ts
+++ src/app/recipes/recipe-list/recipe/recipe.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe',
-  templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+    selector: 'app-recipe',
+    templateUrl: './recipe.component.html',
+    styleUrls: ['./recipe.component.css']
 })
+
 export class RecipeComponent implements OnInit {
+    @Input() recipe: Recipe;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
