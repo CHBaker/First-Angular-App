e7611f433b28a59b35e55a2079d7828b6340ff68
CHBaker <charlesprobaker@mail.com>
2017-06-25 19:48:21 -0600

    ADD ingredients to recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-    
+
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Meat Cereal',
+                   'This is simply the best',
+                   'https://i.ytimg.com/vi/o0_uqcBZ6RU/maxresdefault.jpg',
+                   [
+                       new Ingredient('Meat', 1),
+                       new Ingredient('Lucky Charms (boxes)', 10)
+                   ]),
+        new Recipe('Spaghetti Without Rabbits',
+                   'A delicious medley of noodles and sauce, but hold the rabbits',
+                   'http://del.h-cdn.co/assets/16/04/1600x800/landscape-1453913826-delish-creamy-spaghetti-3.jpg',
+                   [
+                       new Ingredient('Rabbits', 0),
+                       new Ingredient('Spaghetti', 1)
+                   ])
     ];
 
     getRecipes() {
